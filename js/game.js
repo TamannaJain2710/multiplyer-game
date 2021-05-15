719f83f23404b2031ae8f240a01efb3afa035aed
Tamanna <thecreativefeather725@gmail.com>
2021-05-15 17:08:23 +0530

    j

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 class Game{
-    constructor(){}
+    constructor(){
+        
+    }
 
 
     getState(){
@@ -17,17 +19,62 @@ class Game{
    
     }
 
-    start(){
+    async start(){
         if(gameState === 0){
-
             player = new Player()
-            player.getCount();
+            var playerCountRef = await database.ref("playerCount").once("value");
+            if(playerCountRef.exists()){
+            playerCount = playerCountRef.val();
+                player.getCount();
+            }
             form = new Form()
             form.display()
         }
-
-
+        c1= createSprite(100,200);
+        c2= createSprite(300,200);
+        c3= createSprite(500,200);
+        c4= createSprite(700,200);
+        cars= [c1,c2,c3,c4];
     }
 
-   
+    play(){
+
+form.hide();
+textSize(30)
+text("Game Start",120,100)
+
+Player.getPlayerInfo()
+if(allPlayers !==undefined){
+//var display_position = 130;
+var index= 0;
+var x=0;
+var y;
+for(var plr in allPlayers){
+index= index+1;
+x += 200;
+y= displayHeight - allPlayers[plr].distance;
+console.log(plr);
+console.log(index) ;
+cars[index-1].x= x;
+cars[index-1].y= y;
+if(index === player.index)
+{
+cars[index-1].shapeColor="red";
+camera.position.x = displayWidth/2;
+camera.position.y = cars[index-1].y;
+}
+}
+//display_position +=20
+//textSize(15)
+//text(allPlayers[plr].name + ": " +  allPlayers[plr].distance, 120, display_position)
+
+}
+
+if(keyIsDown(UP_ARROW) && player.index !==null){
+player.distance +=50
+player.update()
+    }
+drawSprite();
+
+}
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 var database;
-
+var allPlayers;
 
 var gameState = 0;
 var playerCount;
-
+var c1,c2,c3,c4,cars;
 var form, player, game;
 
 
 function setup(){
-    createCanvas(400,400);
+    createCanvas(displayWidth-20,displayHeight-30);
     database = firebase.database()
 
     game = new Game();
@@ -18,4 +18,14 @@ function setup(){
 
 function draw(){
    // background("white");
+   if(playerCount === 4){
+
+    game.updateState(1);
+
+   }
+if(gameState === 1){
+    clear();
+    game.play();
+}
+   
 }
